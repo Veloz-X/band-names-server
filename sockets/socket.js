2f956a6e03d21b85438024cc4d32de23d2b60bdc
VelozX <viralveloz@gmail.com>
2022-09-06 19:13:08 -0500

    Update socket.js

--- sockets/socket.js
+++ sockets/socket.js
@@ -11,7 +11,7 @@ console.log('INICIANDO SERVIDOR');
 io.on('connection', (client) => {
     console.log('New user connected');
 
-    client.emit('active-bands', bands.getBands());
+    client.emit('active-candidates', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('User disconnected');
@@ -22,24 +22,19 @@ io.on('connection', (client) => {
     });
     client.on('vote-band', (payload) => {
         bands.voteBand(payload.id);
-        io.emit('active-bands', bands.getBands());
+        io.emit('active-candidates', bands.getBands());
         // console.log(payload);
     });
 
     client.on('add-band', (payload) => {
         const newBand = new Band(payload.name);
         bands.addBand(newBand);
-        io.emit('active-bands', bands.getBands());
+        io.emit('active-candidates', bands.getBands());
     });
     
     client.on('delete-band', (payload) => {
         bands.deleteBand(payload.id);
-        io.emit('active-bands', bands.getBands());
+        io.emit('active-candidates', bands.getBands());
     });
 
-    // client.on('emitir-mensaje', (payload) => {
-    //     // console.log('nuevo-mensaje', payload);
-    //     // io.emit('nuevo-mensaje', payload);
-    //     client.broadcast.emit('nuevo-mensaje', payload);
-    // });
 });
